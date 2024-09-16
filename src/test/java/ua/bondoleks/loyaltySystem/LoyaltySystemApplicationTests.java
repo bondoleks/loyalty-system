51fed6d4a3d7a95304e4726227b95d478d52f07a
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2024-09-16 18:47:54 +0300

    Initial commit

---
+++ src/test/java/ua/bondoleks/loyaltySystem/LoyaltySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bondoleks.loyaltySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
